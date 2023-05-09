dc0fa3f0b81fb7d8a46bd9d490a12f1c31e10680
Denis Smirnov <desm80@yandex.ru>
2023-05-09 19:24:50 +0300

    добавил alembic

--- app/core/db.py
+++ app/core/db.py
@@ -9,14 +9,11 @@ class PreBase:
 
     @declared_attr
     def __tablename__(cls):
-        # Именем таблицы будет название модели в нижнем регистре.
         return cls.__name__.lower()
 
-    # Во все таблицы будет добавлено поле ID.
     id = Column(Integer, primary_key=True)
 
 
-# В качестве основы для базового класса укажем класс PreBase.
 Base = declarative_base(cls=PreBase)
 
 engine = create_async_engine(settings.database_url)
@@ -26,12 +23,6 @@ async_session = AsyncSession(engine)
 AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession)
 
 
-# Асинхронный генератор сессий.
 async def get_async_session():
-    # Через асинхронный контекстный менеджер и sessionmaker
-    # открывается сессия.
     async with AsyncSessionLocal() as async_session:
-        # Генератор с сессией передается в вызывающую функцию.
         yield async_session
-        # Когда HTTP-запрос отработает - выполнение кода вернётся сюда,
-        # и при выходе из контекстного менеджера сессия будет закрыта.
