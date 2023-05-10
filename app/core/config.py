860e154f5f6efe0e34b957e192bcdd4218807a3c
desm <desm80@yandex.ru>
2023-05-10 13:06:21 +0300

    настроены роутеры. приложение стартует.

--- app/core/config.py
+++ app/core/config.py
@@ -4,7 +4,7 @@ from pydantic import BaseSettings, EmailStr
 
 
 class Settings(BaseSettings):
-    app_title: str = 'Благотворительный фонд QRCot'
+    app_title: str = 'Благотворительный фонд'
     description: str = 'Какое-то описание'
     database_url: str = 'sqlite+aiosqlite:///./fastapi.db'
     secret: str = 'SECRET'
