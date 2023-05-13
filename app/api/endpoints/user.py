4a6d662625e068141887a52e887f145eb37ba906
Denis Smirnov <desm80@yandex.ru>
2023-05-13 09:11:55 +0300

    первое ревью

--- app/api/endpoints/user.py
+++ app/api/endpoints/user.py
@@ -8,8 +8,6 @@ from app.schemas.user import UserCreate, UserRead, UserUpdate
 router = APIRouter()
 
 router.include_router(
-    # В роутер аутентификации
-    # передается объект бэкенда аутентификации.
     fastapi_users.get_auth_router(auth_backend),
     prefix='/auth/jwt',
     tags=['auth'],
@@ -26,18 +24,14 @@ router.include_router(
 )
 
 
-# В самом конце файла допишите свой эндпоинт.
 @router.delete(
-    # Путь и тег полностью копируют параметры эндпоинта по умолчанию.
     '/users/{id}',
     tags=['users'],
-    # Параметр, который показывает, что метод устарел.
     deprecated=True
 )
 def delete_user(id: str):
     """Не используйте удаление, деактивируйте пользователей."""
     raise HTTPException(
-        # 405 ошибка - метод не разрешен.
         status_code=HTTPStatus.METHOD_NOT_ALLOWED,
         detail="Удаление пользователей запрещено!"
     )
